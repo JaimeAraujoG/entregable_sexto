5aa668b39d9dd7d003fc1fea02473fd1650f0678
Jaime <araujoguerrerojaimeandres@gmail.com>
2024-02-20 22:00:18 -0500

    martes:test con autorizacion con token

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -2,6 +2,8 @@ const request = require("supertest")
 const app = require("../app")
 
 const URL_BASE = '/users'
+let TOKEN;
+
 const user = {
     firstName: 'camilo',
     lastName: 'lopez',
@@ -10,6 +12,32 @@ const user = {
     phone: '+5145245'
 }
 
+beforeAll(async () => {
+    const user = {
+        email: "john.dejesus@gmail.com",
+        password: "jhon123"
+    }
+    const res = await request(app)
+        .post(`${URL_BASE}/login`)
+        .send(user)
+
+    console.log(res.body);
+    TOKEN = res.body.token
+    console.log(TOKEN);
+})
+
+test("GET -> 'URL_BASE', should return status code 200, res.body to be defined abd res.body.length === 1 ", async () => {
+    const res = await request(app)
+        .get(URL_BASE)
+        .set( {'Authorization': `Bearer ${TOKEN}`} ) 
+
+    
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+})
+    
 
 test("POST -> 'URL_BASE', should return status code 201, res.body to be defined and res.body.firstName === user.firstName", async () => {
     const res = await request(app)
