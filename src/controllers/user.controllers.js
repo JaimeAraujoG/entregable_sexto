aa7c91a0c8c8ea49ea935336276002d5a0db8976
Jaime <araujoguerrerojaimeandres@gmail.com>
2024-02-20 20:54:45 -0500

    martes: instalamos dependencias de jest y supertest

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 const catchError = require('../utils/catchError');
 const User = require('../models/User');
 const jwt = require( 'jsonwebtoken' );
+const bcrypt = require ('bcrypt')
 
 const getAll = catchError(async(req, res) => {
     const results = await User.findAll();
@@ -38,6 +39,10 @@ const login = catchError(async (req, res) => {
 
     const user = await User.findOne({ where: {email} })
     if (!user) return res.status(401).json({ error: "Email or Password incorrect"})
+
+    const isValid = await bcrypt.compare(password,  user.password)
+    if (!isValid) return res.status(401).json({ error: "Email or Password incorrect"})
+
 
     const token = jwt.sign(
         {user},
