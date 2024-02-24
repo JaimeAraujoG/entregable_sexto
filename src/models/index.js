5ba8a61f0818793d9a30fc046229acafc8dee84e
Jaime <araujoguerrerojaimeandres@gmail.com>
2024-02-23 22:51:26 -0500

    viernes: creamos el modelo de purchase y perfeccionamos los controladores

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,7 @@
 const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
+const Purchase = require("./Purchase");
 const User = require("./User");
 
 
@@ -15,3 +16,12 @@ User.hasMany(Cart)
 //relacion modelo cart cart-productId
 Cart.belongsTo(Product)
 Product.hasMany(Cart)
+
+// userID -- purchase
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+//productId --  purchase
+
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
